5355a00138d82f1ef89b9bfe4bed5cb9ea4e3dc4
Filipe Husak <69702727+flp04@users.noreply.github.com>
2024-09-14 21:46:35 -0300

    correcoes, refatoracoes e limpeza de codigo

--- Migrations/20240915004508_removerColunaElementoIdTabelaAfazeres.Designer.cs
+++ Migrations/20240915004508_removerColunaElementoIdTabelaAfazeres.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Modulo.Data;
 
@@ -10,9 +11,10 @@ using Modulo.Data;
 namespace lista_afazeres.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240915004508_removerColunaElementoIdTabelaAfazeres")]
+    partial class removerColunaElementoIdTabelaAfazeres
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20240915004508_removerColunaElementoIdTabelaAfazeres.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace lista_afazeres.Migrations
+{
+    public partial class removerColunaElementoIdTabelaAfazeres : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ElementoId",
+                table: "Afazeres");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ElementoId",
+                table: "Afazeres",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
